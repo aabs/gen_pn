653b464312803027c91a247f7a98df6ab343863a
Andrew Matthews <andrew.matthews@premier.com.au>
2011-02-14 09:03:50 +1100

    prelim support for synch/asynch

--- src/gen_pn.erl
+++ src/gen_pn.erl
@@ -6,11 +6,11 @@
 -include("petrinet.hrl").
 
 behaviour_info(callbacks) ->
-    [{init,3}, {terminate,3}, {code_change,4}];
+	[{init,3}, {terminate,3}, {code_change,4}];
 behaviour_info(_Other) ->
     undefined.
 
-%% Start an anonymous instance of a petri net
+%% @doc Start an anonymous instance of a petri net
 %% Args must be of the form [Places, Transitions, Arcs]
 %% where Places is [{place_name::term(), capacity::int, inhibitor::bool(), initial_marking::int()}]
 %% where Transitions is [{transition_name::term()}]
@@ -33,19 +33,19 @@ init_it(Starter, Parent, Name0, Mod, Args, _Options) ->
 	Name = name(Name0),
     
 	case catch Mod:init(Args, Marking, PN) of
-	{ok, StateData} ->
-	    proc_lib:init_ack(Starter, {ok, self()}), 	    
-	    loop(Parent, Name, Marking, PN, StateData, Mod, infinity);
-	{stop, Reason} ->
-	    proc_lib:init_ack(Starter, {error, Reason}),
-	    exit(Reason);
-	{'EXIT', Reason} ->
-	    proc_lib:init_ack(Starter, {error, Reason}),
-	    exit(Reason);
-	Else ->
-	    Error = {bad_return_value, Else},
-	    proc_lib:init_ack(Starter, {error, Error}),
-	    exit(Error)
+		{ok, StateData} ->
+			proc_lib:init_ack(Starter, {ok, self()}), 	    
+			loop(Parent, Name, Marking, PN, StateData, Mod, infinity);
+		{stop, Reason} ->
+			proc_lib:init_ack(Starter, {error, Reason}),
+			exit(Reason);
+		{'EXIT', Reason} ->
+			proc_lib:init_ack(Starter, {error, Reason}),
+			exit(Reason);
+		Else ->
+			Error = {bad_return_value, Else},
+			proc_lib:init_ack(Starter, {error, Error}),
+			exit(Error)
     end.
 
 %% accessors for names of registered processes
@@ -61,12 +61,20 @@ name(Pid) when is_pid(Pid) -> Pid.
 %%     Lim :: int() - the maximum number of fires to perform before returning to the receive loop
 %%     one - perform a single fire and then return to the receive loop
 %%     infinite - keep firing until there are no more enabled transitions
+%% {signal, Place, From, Args}
+%%     Synchronous signal event. The sender expects to receive the response
+%% 
+%% 
+%% 
+%% 
 loop(Parent, Name, Marking, PN, StateData, Mod, hibernate) ->
     proc_lib:hibernate(?MODULE,wake_hib,
 		[Parent, Name, Marking, PN, StateData, Mod, true]);
 loop(Parent, Name, Marking, PN, StateData, Mod, Time) ->
     Msg = receive
-	      Input ->
+		{'EXIT', Parent, Reason} ->
+		    terminate(Reason, Name, Msg, Mod, OriginalMarking, PetriNet, StateData);
+	    Input ->
 		    Input
 	  after Time ->
 		  {'$gen_event', timeout}
@@ -74,27 +82,44 @@ loop(Parent, Name, Marking, PN, StateData, Mod, Time) ->
     handle_msg(Msg,Parent, Name, Marking, PN, StateData, Mod, Time).
 
 %% dispatches incoming events to the user Mod
-handle_msg(Msg, Parent, Name, Marking, PN, StateData, Mod, Time)->
-	case Msg of
-		{'EXIT', Parent, Reason} ->
-		    terminate(Reason, Name, Msg, Mod, Marking, PN, StateData);
+handle_msg(Msg, Parent, Name, OriginalMarking, PetriNet, StateData, Mod, Time)->
+	Response = case Msg of
 		{signal, Place, Args} ->
-			M2 = petrinet:signal(Place, Marking),
-			X = petrinet:get_fire_list(PN, M2),
-			case X of
-				[non_live] ->
-					loop(Parent, Name, M2, PN, StateData, Mod, Time);
-				[{T, Mn}]->
-					case Mod:T(StateData, PN, Marking, M2, Args) of 
-						{stop, Reason} ->
-							terminate(Reason, Name, [], Mod, M2, PN, StateData);
-						{ok, _Reply, NewStateData}->
-							% not sure what to do with the reply just yet, so just recycle
-							loop(Parent, Name, Mn, PN, NewStateData, Mod, Time)
-					end
+			process_signal(PetriNet, Place, OriginalMarking, Mod, StateData);
+		{signal, Place, Args, From} ->
+			process_signal(PetriNet, Place, OriginalMarking, Mod, StateData);
+		Other ->
+			terminate(unknown_input, Name, Msg, Mod, OriginalMarking, PetriNet, StateData)
+		end,
+	process_reponse(Other, Parent, Name, PetriNet, OriginalMarking, StateData, Mod, Time).		
+
+process_signal(PetriNet, Place, OriginalMarking, Mod, StateData)->
+	SignalledMarking = petrinet:signal(Place, OriginalMarking),
+	FireList = petrinet:get_fire_list(PetriNet, SignalledMarking),
+	case FireList of
+		[non_live] ->
+			no_op;
+		[{T, ResultMarking}]->
+			case Mod:T(StateData, PetriNet, OriginalMarking, ResultMarking, Args) of 
+				{stop, Reason} ->
+					terminate(Reason, Name, [], Mod, OriginalMarking, PetriNet, StateData);
+				Other ->
+					{Other, ResultMarking}
 			end
 	end.
 
+process_response(Response, Parent, Name, PetriNet, OriginalMarking, StateData, Mod, Time)
+	case Response of
+		no_op ->
+			loop(Parent, Name, OriginalMarking, PetriNet, StateData, Mod, Time);
+		{{reply, ReplyMsg, NewStateData}, NewMarking}->
+			% do the reply here...
+			do_reply(From, ReplyMsg),
+			loop(Parent, Name, NewMarking, PetriNet, NewStateData, Mod, Time);
+	end.
+
+do_reply(From, ReplyMsg)->
+	From ! ReplyMsg.
 
 %%---------------------------------------------
 %% stuff lifted straight out of gen_fsm
@@ -113,46 +138,47 @@ terminate(Reason, Name, Msg, Mod, Marking, PetriNet, StateData) ->
  		{shutdown,_}=Shutdown ->
  		    exit(Shutdown);
 		_ ->
-                    FmtStateData =
-                        case erlang:function_exported(Mod, format_status, 2) of
-                            true ->
-                                Args = [get(), StateData],
-                                case catch Mod:format_status(terminate, Args) of
-                                    {'EXIT', _} -> StateData;
-                                    Else -> Else
-                                end;
-                            _ ->
-                                StateData
+			FmtStateData =
+				case erlang:function_exported(Mod, format_status, 2) of
+					true ->
+						Args = [get(), StateData],
+						case catch Mod:format_status(terminate, Args) of
+							{'EXIT', _} -> StateData;
+							Else -> Else
+						end;
+					_ ->
+						StateData
                         end,
 		    error_info(Reason,Name,Msg,Marking, PetriNet,FmtStateData),
 		    exit(Reason)
 	    end
     end.
+
 error_info(Reason, Name, Msg, Marking, PetriNet, StateData) ->
-    Reason1 = 
-	case Reason of
+    Reason1 = case Reason of
 	    {undef,[{M,F,A}|MFAs]} ->
-		case code:is_loaded(M) of
-		    false ->
-			{'module could not be loaded',[{M,F,A}|MFAs]};
-		    _ ->
-			case erlang:function_exported(M, F, length(A)) of
-			    true ->
-				Reason;
-			    false ->
-				{'function not exported',[{M,F,A}|MFAs]}
+			case code:is_loaded(M) of
+				false ->
+					{'module could not be loaded',[{M,F,A}|MFAs]};
+				_ ->
+					case erlang:function_exported(M, F, length(A)) of
+						true ->
+							Reason;
+						false ->
+						{'function not exported',[{M,F,A}|MFAs]}
 			end
 		end;
 	    _ ->
-		Reason
+			Reason
 	end,
-    Str = "** State machine ~p terminating \n" ++
+    Str = "** Petri Net ~p terminating \n" ++
 	get_msg_str(Msg) ++
-	"** When State == ~p~n"
+	"** When Marking == ~p~n"
         "**      Data  == ~p~n"
         "** Reason for termination = ~n** ~p~n",
-    format(Str, [Name, get_msg(Msg), Marking, PetriNet, StateData, Reason1]),
+    format(Str, [Name, Marking, StateData, Reason1]),
     ok.
+
 get_msg_str({'$gen_event', _Event}) ->
     "** Last event in was ~p~n";
 get_msg_str({'$gen_sync_event', _Event}) ->
